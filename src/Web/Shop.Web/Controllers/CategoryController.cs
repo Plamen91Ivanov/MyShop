43f9259395cce33f80a19a61b2061169746cfb8c
plamen <plamen91ivanov@abv.bg>
2021-02-25 17:24:41 +0200

    k

---
+++ src/Web/Shop.Web/Controllers/CategoryController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Web.Controllers
+{
+    public class CategoryController : BaseController
+    {
+        public IActionResult categoryByName(string name)
+        {
+
+            return this.View();
+        }
+    }
+}
